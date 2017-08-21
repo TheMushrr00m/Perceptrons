2ef11ed92d2f958668d794daf3da6f30a9fe7e7f
Ethan Peacock <peacockethan@gmail.com>
2017-08-21 00:02:06 -0500

    Created variables

---
+++ Main.py
@@ -0,0 +1,16 @@
+# In[0]: <- for Hydrogen (to use jupyter kernels in Atom)
+# import statements
+import pandas as pd 
+
+print('...')
+
+
+# In[1]: set weight1, weight2, and bias
+weight1 = 0.0
+weight2 = 0.0
+bias    = 0.0
+
+print('...')
+
+
+# In[2] Inputs and Outputs
