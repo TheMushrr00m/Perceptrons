fdd3575db4de61179453b6745bdf363831c4ab75
Ethan Peacock <peacockethan@gmail.com>
2017-08-21 00:10:03 -0500

    for loop to iterate through two parallel lists using zip to evaulate linear_combination

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # In[0]: <- for Hydrogen (to use jupyter kernels in Atom)
 # import statements
-import pandas as pd 
+import pandas as pd
 
 print('...')
 
@@ -14,3 +14,15 @@ print('...')
 
 
 # In[2] Inputs and Outputs
+test_inputs = [(0,0,), (0,1), (1, 0), (1, 1)]
+correct_outputs = [False, False, False, True]
+outputs = []
+
+print('...')
+
+
+# In[3]:  Generate and check output
+for test_input, correct_output in zip(test_inputs, correct_outputs):
+    linear_combination = weight1 * test_input[0] + weight2 * test_input[1] + bias
+    output = int(linear_combination >= 0)
+    
